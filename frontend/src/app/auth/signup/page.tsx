b3183a2d469bd1b83157e2abd0b1a0123890c6b3
daleh <51839961+alsubaieedaleh@users.noreply.github.com>
2024-10-04 21:53:48 +0300

    auth done!

--- frontend/src/app/auth/signup/page.tsx
+++ frontend/src/app/auth/signup/page.tsx
@@ -2,7 +2,8 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-
+import Image from 'next/image';
+import background from '../../public/images/MacBook Pro 14_ - 1.svg';
 const SignUp = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
@@ -26,64 +27,75 @@ const SignUp = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
+    <div className="relative h-screen w-full overflow-hidden text-right">
+  
+    <Image
+      src={background}
+      alt="Background Cover"
+      fill
+      className="-z-10 object-cover  "
+    />
+    <div className="flex items-center justify-center min-h-screen ">
+    <div className="bg-white/10 p-16 rounded-[10%] shadow-lg w-[470px]  mr-1 ">
+    <h2 className="text-2xl font-bold mb-6 text-right" > ✋ مرحبًا بك     </h2>
+
+    <h2 className="text-5xl font-bold mb-6 text-right">إنشاء حساب</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block mb-2 text-sm font-bold text-gray-700">
-              Name
+          <label className="block mb-2  text-lg font-base text-white/80">
+          اسمك الكريم
             </label>
             <input
               type="text"
               value={name}
               onChange={(e) => setName(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              placeholder="Enter your name"
+              className="w-full px-3 py-2 border text-right border-gray-300 rounded-md"
+              placeholder="أدخل اسمك"
               required
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2 text-sm font-bold text-gray-700">
-              Email
+          <label className="block mb-2  text-lg font-base text-white/80">
+          ايميلك
             </label>
             <input
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              placeholder="Enter your email"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md text-right"
+              placeholder="أدخل بريدك الإلكتروني"
               required
             />
           </div>
           <div className="mb-6">
             <label className="block mb-2 text-sm font-bold text-gray-700">
-              Password
+              كلمة المرور
             </label>
             <input
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              placeholder="Enter your password"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md text-right"
+              placeholder="أدخل كلمة المرور الخاصة بك"
               required
             />
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-200"
+            className="w-full bg-secondary text-white py-2 px-4 rounded-md hover:bg-primary transition duration-200"
           >
-            Sign Up
+           إنشاء حساب
           </button>
         </form>
-        <p className="text-center text-gray-500 mt-4">
-          Already have an account?{' '}
-          <a href="/auth/signin" className="text-blue-500 hover:underline">
-            Sign In
+        <p className="text-center text-gray-100 mt-4">
+          لديك حساب بالفعل؟{' '}
+          <a href="/auth/signin" className="text-blue-100 text-lg font-bold hover:underline">
+            سجل الدخول
           </a>
         </p>
       </div>
-    </div>
+      </div>
+      </div>
   );
 };
 
