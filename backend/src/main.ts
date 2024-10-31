516b6b54e7e9ba887a5b2945452b5127ef9aa3bc
daleh <51839961+alsubaieedaleh@users.noreply.github.com>
2024-11-01 00:32:24 +0300

    docker2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     credentials: true,  // Enable cookies and other credentials if needed
   });
   
-  await app.listen(3000, hostname: '0.0.0.0');
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
