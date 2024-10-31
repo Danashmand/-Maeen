c914b1ceb37814b4fcb9f688cbbda2982f48f042
daleh <51839961+alsubaieedaleh@users.noreply.github.com>
2024-11-01 01:08:49 +0300

    backend uploded

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,11 @@ import { hostname } from 'os';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001',"*"], // Specify your frontend URL(s)
+    origin: 'http://localhost:3001', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true, // Enable credentials
     preflightContinue: false, // Whether to pass the CORS preflight response to the next handler
-    optionsSuccessStatus: 204, // Provide a status code for successful OPTIONS requests
+    optionsSuccessStatus: 200, // Provide a status code for successful OPTIONS requests
   });
   
   await app.listen(process.env.PORT, '0.0.0.0');
